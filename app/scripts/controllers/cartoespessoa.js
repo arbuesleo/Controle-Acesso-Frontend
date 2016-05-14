3b21a08d1d5211baa92fc5b8d480149532bc0e50
Leonardo Gomes <leogomes26@hotmail.com>
2016-05-14 20:18:29 -0300

    Modal, pesquisa de cartões.

--- app/scripts/controllers/cartoespessoa.js
+++ app/scripts/controllers/cartoespessoa.js
@@ -10,10 +10,10 @@
 angular.module('controleAcessoAppApp')
   .controller('CartoespessoaCtrl', function ($scope, $http, loadEditPessoa) {
     var idPessoa = loadEditPessoa.getId();
-    $scope.cartoesPessoa;
+    $scope.pessoa;
     $scope.loadCartoesPessoa = function () {
       $http.get("http://localhost:8888/pessoa/finbById?id="+ idPessoa).then(function(response){
-          $scope.cartoesPessoa = response.data.cartoes;
+          $scope.pessoa = response.data;
       }, function(response){
            $scope.name = "Ocorreu um erro ao se conectar ao servidor";
       }
@@ -22,4 +22,25 @@ angular.module('controleAcessoAppApp')
 
   $scope.loadCartoesPessoa();
 
+  $scope.removeCartao = function (idCartao) {
+
+    $http.get("http://localhost:8888/cartao/removePessoaCartao?id="+ idCartao).then(function(response){
+      $scope.loadCartoesPessoa();
+    }, function(response){
+         $scope.name = "Ocorreu um erro ao se conectar ao servidor";
+    }
+  );
+}
+
+$scope.getCartoesDisp = function () {
+
+  $http.get("http://localhost:8888/cartao/findDisponiveis").then(function(response){
+
+    $("#modalListCartoes").modal('show');
+    $scope.cartoesDisponiveis = response.data;
+  }, function(response){
+       $scope.name = "Ocorreu um erro ao se conectar ao servidor";
+  }
+);
+}
   });
