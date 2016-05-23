772ebb6d66f3eed94af2dcecd7b97095146fa100
Leonardo Gomes Jorge <leogomes26@hotmail.com>
2016-05-23 15:25:02 -0300

    Grafico tela inicial.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,28 +10,85 @@
 angular.module('controleAcessoAppApp')
   .controller('MainCtrl', function ($scope, $http) {
     var options = {
-                    responsive:true
-                };
+      responsive: true,
+      //Boolean - Whether to show lines for each scale point
+scaleShowLine : true,
+
+//Boolean - Whether we show the angle lines out of the radar
+angleShowLineOut : true,
+
+//Boolean - Whether to show labels on the scale
+scaleShowLabels : true  ,
+
+// Boolean - Whether the scale should begin at zero
+scaleBeginAtZero : true,
+
+//String - Colour of the angle line
+angleLineColor : "rgba(1,1,1,.1)",
+
+//Number - Pixel width of the angle line
+angleLineWidth : 1,
+
+//String - Point label font declaration
+pointLabelFontFamily : "'Arial'",
+
+//String - Point label font weight
+pointLabelFontStyle : "normal",
+
+//Number - Point label font size in pixels
+pointLabelFontSize : 5,
+
+//String - Point label font colour
+pointLabelFontColor : "#555",
+
+//Boolean - Whether to show a dot for each point
+pointDot : true,
+
+//Number - Radius of each point dot in pixels
+pointDotRadius : 4,
+
+//Number - Pixel width of point dot stroke
+pointDotStrokeWidth : 2,
+
+//Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+pointHitDetectionRadius : 20,
+
+//Boolean - Whether to show a stroke for datasets
+datasetStroke : true,
+
+//Number - Pixel width of dataset stroke
+datasetStrokeWidth : 3,
+
+//Boolean - Whether to fill the dataset with a colour
+datasetFill : true
+};
       var horas = ["teste"];
       var acessos = ["22"];
     $scope.carregraGrafico = function (){
-                    $http.get("http://localhost:8888/acesso/groupHora?dtIn=19/05/2016&dtFim=22/05/2016").then(function(response){
+                    var dataFinal = new Date();
+                    var dataInicial = subtrairDias(dataFinal, 7).toLocaleDateString();
+                    dataFinal = dataFinal.toLocaleDateString();
+                    $http.get("http://localhost:8888/acesso/groupHora?dtIn="+dataInicial+"&dtFim="+dataFinal).then(function(response){
 
                          for (var i = 0; i < response.data.length; i++) {
                             acessos[i] = response.data[i][0];
-                            horas[i] = response.data[i][1];
+                            horas[i] = response.data[i][1] + " Hr";
                          }
+                         var tituo ={
+                              display: true,
+                              text: "Tste"
+                            };
                          var data = {
                              labels: horas,
                              datasets: [
                                  {
-                                     label: "Dados primários",
-                                     fillColor: "rgba(220,220,220,0.2)",
-                                     strokeColor: "rgba(220,220,220,1)",
-                                     pointColor: "rgba(220,220,220,1)",
-                                     pointStrokeColor: "#fff",
-                                     pointHighlightFill: "#fff",
-                                     pointHighlightStroke: "rgba(220,220,220,1)",
+                                     label: "Acessos",
+                                     fillColor: "rgba(72,61,139,0.8)", // preenchimento
+                                     strokeColor: "rgba(72,61,139,100)", // linha
+                                     pointColor: "rgba(165,42,42,100)", //ponto
+                                     pointStrokeColor: "rgba(165,42,42,100)", //contorno ponto
+                                     pointHighlightFill: "#696969", // cor ponto ao passar o mouse
+                                     pointHighlightStroke: "#D3D3D3", // contono do ponto ao passar o mouse
                                      data: acessos
                                  }
                              ]
@@ -51,7 +108,9 @@ angular.module('controleAcessoAppApp')
 
     $scope.carregraGrafico();
 
-
+    function subtrairDias(data, dias){
+    return new Date(data.getTime() - (dias * 24 * 60 * 60 * 1000));
+ }
 
 
   });
